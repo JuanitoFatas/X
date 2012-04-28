12aea1ff5daa755c78814509304b2173a0d8ead2
Hsin-Hsiang Huang <katehuang0320@gmail.com>
2012-04-28 12:31:39 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XWeibo::Application.config.secret_token = 'af56f9132a1dfcf0b33550292d634a53413c71dcb966184a67881e64ef919615d8a5cc5c4d2b725dba44441acefd7bdc21ed55c04ab855e26c1eceda6e29edc3'
